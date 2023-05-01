51dd74d80793bce4cfb07dd33492b5a7a82ad498
TheBacou <69599039+TheBacou@users.noreply.github.com>
2023-05-01 17:39:23 +0200

    function changeIcon

--- assets/js/navIconChange.js
+++ assets/js/navIconChange.js
@@ -1,4 +1,4 @@
-module.export = (icon) => {
+function navIconChange(icon) {
     let newIcon = icon.className === 'fa fa-bars icon' ? 'fa fa-close icon' : 'fa fa-bars icon'
     icon.className = newIcon
 }
