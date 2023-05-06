6f013b0d78f759fcbbfccb84ef331d2815c2ceba
TheBacou <69599039+TheBacou@users.noreply.github.com>
2023-05-06 23:39:01 +0200

    Bio and Description auto update

--- assets/js/repositories.js
+++ assets/js/repositories.js
@@ -14,29 +14,31 @@ function navIconChange() {
     }
 }
 
-function bio() {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4) {
-            var data = JSON.parse(xhr.response)
-            document.getElementsByName("bio")[0].innerText = data.bio
-        }
-    }
-    xhr.open('GET', "https://api.github.com/users/RedGear-Studio", true);
-    xhr.send();
-}
 
-function repositoriesDescription() {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4) {
-            var data = JSON.parse(xhr.response)
-            for (var i in data) {
-                const elemet = document.getElementByName("description-"+data[i].name) || null
-                if (element) element.innerText = data[i].description
-            }
+
+// Auto update the bio on the Title page
+const xhr = new XMLHttpRequest();
+xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+        var data = JSON.parse(xhr.response)
+        document.getElementsByName("bio")[0].innerText = data.bio
+    }
+}
+xhr.open('GET', "https://api.github.com/users/RedGear-Studio", true);
+xhr.send();
+
+
+
+// Auto update the description on the Main project
+const xhr = new XMLHttpRequest();
+xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+        var data = JSON.parse(xhr.response)
+        for (var i in data) {
+            const elemet = document.getElementByName("description-" + data[i].name) || null
+            if (element) element.innerText = data[i].description
         }
     }
-    xhr.open('GET', "https://api.github.com/users/RedGear-Studio/repos", true);
-    xhr.send();
 }
+xhr.open('GET', "https://api.github.com/users/RedGear-Studio/repos", true);
+xhr.send();
