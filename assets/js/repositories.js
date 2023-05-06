e069234741d1765ca3a23522b773b85992402c9b
TheBacou <69599039+TheBacou@users.noreply.github.com>
2023-05-06 23:06:48 +0200

    Just a commit for the next commit

--- assets/js/repositories.js
+++ assets/js/repositories.js
@@ -13,3 +13,17 @@ function navIconChange() {
         finish.style.display = "block"
     }
 }
+
+module.exports = {
+    bio() {
+        const xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+                var data = JSON.parse(xhr.response)
+                document.getElementById("news-lead").innerHTML
+            }
+        }
+        xhr.open('GET', "https://api.github.com/users/RedGear-Studio", true);
+        xhr.send();
+    }
+}
