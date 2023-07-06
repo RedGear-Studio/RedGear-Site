829d99976123cdef01d18a8a56d81b432e986b60
TheBacou <69599039+TheBacou@users.noreply.github.com>
2023-07-06 12:45:07 +0200

    Hmm ?

--- assets/js/repository.js
+++ assets/js/repository.js
@@ -11,6 +11,25 @@ jQuery(window).on("load", function() {
 
 
 
+// Auto update the description on the Main project
+const xhr2 = new XMLHttpRequest();
+xhr2.onreadystatechange = () => {
+    if (xhr2.readyState === 4) {
+        var data = JSON.parse(xhr2.response)
+        var repoList = []
+        for (var i in data) {
+            if (repository.list.includes(data[i].name)) {
+
+                repoList.push(html(data[i].name, data[i].description))
+            }
+        }
+        document.getElementsByName("mainprojects")[0].innerHTML = repoList.join("")
+    }
+}
+xhr2.open('GET', "https://api.github.com/users/RedGear-Studio/repos", true);
+xhr2.send();
+
+
 // Auto update the bio on the Title page
 const xhr = new XMLHttpRequest();
 xhr.onreadystatechange = () => {
@@ -23,34 +42,3 @@ xhr.onreadystatechange = () => {
 xhr.open('GET', "https://api.github.com/users/RedGear-Studio", true);
 xhr.send();
 
-
-
-// Auto update the description on the Main project
-console.log("r")
-const xhr2 = new XMLHttpRequest();
-console.log("r2")
-xhr2.onreadystatechange = () => {
-    console.log(load)
-    if (xhr2.readyState === 4) {
-        var data = JSON.parse(xhr2.response)
-        var repoList = []
-        console.log(repoList)
-        for (var i in data) {
-            console.log(data[i].name)
-            if (repository.list.includes(data[i].name)) {
-                console.log("pushed")
-
-                repoList.push(html(data[i].name, data[i].description))
-            }
-        }
-        console.log(repoList)
-        console.log(document.getElementsByName("mainprojects")[0])
-        document.getElementsByName("mainprojects")[0].innerHTML = repoList.join("")
-        console.log(document.getElementsByName("mainprojects")[0])
-    }
-}
-console.log("r3")
-xhr2.open('GET', "https://api.github.com/users/RedGear-Studio/repos", true);
-console.log("get")
-xhr2.send();
-console.log("send")
