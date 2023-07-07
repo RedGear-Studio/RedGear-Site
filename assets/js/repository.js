e9c33a027de42062f2b7675ca7062d726b441ee5
TheBacou <69599039+TheBacou@users.noreply.github.com>
2023-07-07 12:10:22 +0200

    Update repository.js
    Auto name, description, and icon

--- assets/js/repository.js
+++ assets/js/repository.js
@@ -1,15 +1,36 @@
-const repository = null
-const fetchRepository = async () => {
-  const result = await fetch("https://redgear.tk/assets/json/repository.json").then(rep => rep.json());
-  repository = await result
-}
-fetchRepository()
-
-
-function html(name, description) {
+function html(repository, name, description) {
     var content = repository.html.join("")
-    return content.replace("{name}", name).replace("{description}", description)
+    var icon = repository.icon["null"]
+    if (repository.icon[name] !== null) icon = repository.icon[name]
+    return content.replace("{icon}", icon).replace("{name}", name).replace("{description}", description)
 }
+
+(async () => {
+    const repositoryFetch = await fetch("https://redgear.tk/assets/json/repository.json").then(rep => rep.json());
+    const repository = {name: [], icon: {"null": repositoryFetch.icons["null"] }, html: repositoryFetch.html}
+    function splitRepoList(value) {
+      repository.name.push(value.name)
+      repository.icon[value.name] = value.icon
+    }
+    await repositoryFetch.list.forEach(splitRepoList());
+
+    const xhr2 = new XMLHttpRequest();
+    xhr2.onreadystatechange = async () => {
+        if (xhr2.readyState === 4) {
+            var data = JSON.parse(xhr2.response)
+            var repoList = []
+            for (var i in data) {
+                if (repository.name.includes(data[i].name)) {
+					var content = await html(repository, data[i].name, data[i].description)
+                    repoList.push(content)
+                }
+            }
+            document.getElementsByName("mainprojects")[0].innerHTML = repoList.join("")
+        }
+    }
+    xhr2.open('GET', "https://api.github.com/users/RedGear-Studio/repos", true);
+    xhr2.send();
+})()
 
 
 jQuery(window).on("load", function() {
@@ -29,23 +50,3 @@ xhr.onreadystatechange = () => {
 }
 xhr.open('GET', "https://api.github.com/users/RedGear-Studio", true);
 xhr.send();
-
-
-
-// Auto update the description on the Main project
-const xhr2 = new XMLHttpRequest();
-xhr2.onreadystatechange = () => {
-    if (xhr2.readyState === 4) {
-        var data = JSON.parse(xhr2.response)
-        var repoList = []
-        for (var i in data) {
-            if (repository.list.includes(data[i].name)) {
-
-                repoList.push(html(data[i].name, data[i].description))
-            }
-        }
-        document.getElementsByName("mainprojects")[0].innerHTML = repoList.join("")
-    }
-}
-xhr2.open('GET', "https://api.github.com/users/RedGear-Studio/repos", true);
-xhr2.send();
