f8265592122b719cd5c43f3cafaa77ac25caa68b
TheBacou <69599039+TheBacou@users.noreply.github.com>
2024-09-08 18:24:53 +0200

    Create incidents.js

---
+++ assets/js/incidents.js
@@ -0,0 +1,17 @@
+function incidents(it) {
+    var expand = it.attributes["aria-expanded"].value;
+    if (expand === "false") it.setAttribute("aria-expanded", "true")
+    else it.setAttribute("aria-expanded", "false")
+}
+
+
+const xhr3 = new XMLHttpRequest();
+xhr3.onreadystatechange = () => {
+    if (xhr3.readyState === 4) {
+        var data = xhr3.response
+        console.log(data)
+        document.getElementById("latest-incidents").innerHTML += data
+    }
+}
+xhr3.open('GET', "http://node1.adky.net:3034/incidents", true);
+xhr3.send();
