6be73e5adeb1f44c24b94a857ace7563c4faa199
TheBacou <69599039+TheBacou@users.noreply.github.com>
2024-09-08 18:54:15 +0200

    Update incidents.js

--- assets/js/incidents.js
+++ assets/js/incidents.js
@@ -9,9 +9,8 @@ const xhr3 = new XMLHttpRequest();
 xhr3.onreadystatechange = () => {
     if (xhr3.readyState === 4) {
         var data = xhr3.response
-        console.log(data)
         document.getElementById("latest-incidents").innerHTML += data
     }
 }
-xhr3.open('GET', "node1.adky.net:3034/incidents/", true);
+xhr3.open('GET', "node1.adky.net:3034/incidents", true);
 xhr3.send();
