8348243fdc3a6fe604780ec470de2c76760e9c33
TheBacou <69599039+TheBacou@users.noreply.github.com>
2024-09-11 20:08:15 +0200

    Update incidents.js

--- assets/js/incidents.js
+++ assets/js/incidents.js
@@ -8,11 +8,41 @@ function incidents(it) {
 const xhr3 = new XMLHttpRequest();
 xhr3.onreadystatechange = () => {
     if (xhr3.readyState === 4) {
-        console.log(xhr3)
-        console.log(xhr3.response)
-        console.log(xhr3.responseText)
-
+        var data = JSON.parse(xhr3.response)
+        // document.getElementById(this.)
+        data = data.msg
+        for (var i in data) {
+            var msg = message()
+            msg = msg.replace("${avatar}", data[i].avatar)
+            msg = msg.replace("${username}", data[i].username)
+            msg = msg.replace("${date}", data[i].timestamp)
+            msg = msg.replace("${content}", data[i].content)
+            document.getElementById("latest-incidents").innerHTML += msg
+        }
     }
 }
-xhr3.open('GET', "http://node1.adky.net:3034/incidents", true);
+xhr3.open('GET', "https://thebacou.github.io/Site-Okoura/testRedgear.json", true); /* http://node1.adky.net:3034/incidents */
 xhr3.send();
+
+
+
+function message() {
+    return `<div class="accordion-item">` +
+    `<button id="accordion-button" aria-expanded="false" onclick="incidents(this)">` +
+    '<img src="${avatar}" class="profil">' +
+    `<span>` +
+    '${username}' +
+    `</span>` +
+    `<span class="accordion-subtitle" id="accordion-subtitle-">` +
+    `- ` +
+    `</span>` +
+    `<span class="accordion-subtitle">` +
+    'Incident of ${date}' +
+    `</span>` +
+    `<span class="icon" aria-hidden="true"></span>` +
+    `</button>` +
+    `<div class="accordion-content">` +
+    '<p>${content}</p>' +
+    `</div>` +
+    `</div>`
+}
